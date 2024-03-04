5d5d740cf0be68b35a773aedfc3cd89b6250c2aa
milochase13 <milochase13@gmail.com>
2024-03-03 23:41:56 -0800

    outline RAG implementation

---
+++ backend/app/providers/impl/getSongOptionsRedisRagLyricsImpl.py
@@ -0,0 +1,25 @@
+'''
+Load lyrics data for each saved song (preloading, should be done async at signin)
+'''
+
+'''
+Ingest the data into running Redis instance (preloading, should be done async at signin)
+'''
+
+'''
+Connect to pre-loaded vectorstore and instantiate a retriever with it
+'''
+
+'''
+Create RAG chain with LangChain
+'''
+
+'''
+Implement get_songs api using the RAG chain
+'''
+
+'''
+References:
+1. https://redis.com/blog/announcing-langchain-rag-template-powered-by-redis/
+2. https://medium.com/@kv742000/creating-a-python-server-for-document-q-a-using-langchain-31a123b67935
+'''
